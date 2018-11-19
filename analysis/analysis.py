5a393f6748b5c238812f4ea82eab9b26f0108d5e
mark-wilmes <mawi0949@colorado.edu>
2018-11-19 14:51:06 -0700

    Formatting data to prepare it for NN
    New methods to format and prepare data to be learned from. 9 inputs, 3 outputs, and formatted training data

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,8 +1,10 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg
 import json
 from prediction import *
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 
 APP_NAME = "Social Media as an Economic Indicator"
@@ -17,11 +19,35 @@ def sentiment_scan(sentiments,s): # define this as a udf, than put it inside a w
 			acc += float(value)
 	return [acc]
 
+def split_by_day(data,filter_days):
+	# time = datetime.strptime(data['created_at'])
+	# returns an array, where one side is sentiment for day1, next for day2, third for day3
+	# sentiment calculated in this function
 
-def assign_sentiment(sc,tweets,sentiments):
-	tweets = tweets.filter(tweets.text.contains('apple') | tweets.text.contains('Apple') | tweets.text.contains('tim cook') | tweets.text.contains('Tim Cook'))
-	dfSent = tweets.rdd.map(lambda x: sentiment_scan(sentiments,x.text)).toDF().selectExpr("_1 as sentiments")
-	return tweets.join(dfSent)
+	day1 = data.filter(datetime.strptime(data.created_at).strftime('%Y-%m-%d') == filter_days[0])
+	day2 = data.filter(datetime.strptime(data.created_at).strftime('%Y-%m-%d') == filter_days[1]) # may not work!!!!!
+	day3 = data.filter(datetime.strptime(data.created_at).strftime('%Y-%m-%d') == filter_days[2])
+
+	df1 = day1.rdd.map(lambda x: sentiment_scan(sentiments,x.text)).toDF().selectExpr("_1 as sentiments").toPandas()
+	df2 = day2.rdd.map(lambda x: sentiment_scan(sentiments,x.text)).toDF().selectExpr("_1 as sentiments").toPandas()
+	df3 = day3.rdd.map(lambda x: sentiment_scan(sentiments,x.text)).toDF().selectExpr("_1 as sentiments").toPandas()
+
+	
+	return [df1['sentiments'].mean(),df2['sentiments'].mean().df3['sentiments'].mean()]
+
+
+def assign_sentiment(sc,tweets,sentiments,days):
+	tweets = tweets.filter(tweets.user['screen_name'].contains('tim_cook') | tweets.text.contains('apple') | tweets.text.contains('Apple') | tweets.text.contains('tim cook') | tweets.text.contains('Tim Cook'))
+	execs = tweets.filter(tweets.user['screen_name'].contains('tim_cook'))
+	generalPublic = tweets.filter(not tweets.user['screen_name'].contains('tim_cook'))
+	training_data = []
+	for i in range(len(days)-2):
+		execSplit = split_by_day(execs,days[i:i+3])
+		tweetSplit = split_by_day(generalPublic,days[i:i+3])
+		tweetSplit2 = split_by_day(generalPublic,days[i:i+3])
+		training_data.append([execSplit + tweetSplit + tweetSplit2])	
+	# dfGeneral = tweets.rdd.map(lambda x: sentiment_scan(sentiments,x.text)).toDF().selectExpr("_1 as sentiments")
+	return training_data
 	
 
 def get_stock_labels(stock_data):
@@ -38,6 +64,8 @@ def bin_tweets(tweets):
 		count += 0.2
 	return bins
 
+#def 
+
 
 if __name__ == '__main__':
 #	conf = SparkConf().setAppName(APP_NAME)
@@ -49,40 +77,37 @@ if __name__ == '__main__':
 	filename = "/opt/output.json"
 	sentiments = "/opt/sentiments.csv"
 	stocks = "/opt/stock.csv"
+	days = ['2015-12-1','2015-12-2','2015-12-3','2015-12-4','2015-12-7','2015-12-8','2015-12-9','2015-12-10','2015-12-11','2015-12-14','2015-12-15',
+	'2015-12-16','2015-12-17','2015-12-18','2015-12-21','2015-12-22','2015-12-23','2015-12-24','2015-12-28','2015-12-29','2015-12-30','2015-12-31']
 
 	sentiment = pd.read_csv(sentiments) # spark.read.csv("file://" + sentiments)
 
 	stock_data = spark.read.csv("file://" + stocks) # parse_stock_data(sc,stocks)
 	tweet_data = spark.read.json("file://" + filename)
-	raw_data = assign_sentiment(sc,tweet_data,sentiment)
+	train_tweets = assign_sentiment(sc,tweet_data,sentiment,days)
 
 	labels = get_stock_labels(stock_data)
-	tweet_bins = bin_tweets(raw_data)
+	#tweet_bins = bin_tweets(raw_data)
 
-	train_tweets = np.array([raw_data.toPandas().values.flatten()])
-	train_stocks = labels.toPandas()['diff'].values
-	for i in range(len(train_stocks)):
-		val = float(train_stocks[i])
+	# train_tweets = np.array([raw_data.toPandas().values.flatten()])
+	raw_stocks = labels.toPandas()['diff'].values
+	train_stocks = []
+	for i in range(len(raw_stocks)):
+		val = float(raw_stocks[i])
 		if(val < 0.2 and val > -0.2):
-			train_stocks[i] = 0
+			raw_stocks[i] = 0
 		elif(val > 0):
-			train_stocks[i] = 1
+			raw_stocks[i] = 1
 		else:
-			train_stocks[i] = -1
+			raw_stocks[i] = -1
+	
+	for i in range(len(raw_stocks)-2):
+		train_stocks.append([raw_stocks[i],raw_stocks[i+1],raw_stocks[i+2]])
 
 	p = predict()
 	p.neural_net()
 	for i in range(31): # loop through every day and update neural network based on new data
 		# filter tweet data based on day here
 		# filter stock data based on day here
-		p.train_network(train_tweets,np.array([[train_stocks[0],train_stocks[1],train_stocks[2]]]) # apply new filtered data for stocks and tweets here
+		p.train_network(np.array(train_tweets),np.array(train_stocks)) # apply new filtered data for stocks and tweets here
 	# run tests on data that has been set aside for testing here
-
-	
-
-	
-	
-	
-
-
-
