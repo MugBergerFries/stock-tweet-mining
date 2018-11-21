906e814b1d67370a49040747efd74d4a1788c51c
mark-wilmes <mawi0949@colorado.edu>
2018-11-21 13:58:49 -0700

    Saving and loading networks from files

--- analysis/prediction.py
+++ analysis/prediction.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.models import model_from_json
 import numpy as np
 
 
@@ -57,3 +58,20 @@ class predict:
         # tweets = np.array(train_tweets)
         # create array based on first 3 days, split the data into 1d array for every 3 days. Can also overlap days
         self.model.fit(train_tweets,train_stock,epochs=1000)
+
+        # Save the model to model.json and model.h5
+        model_json = self.model.to_json()
+        with open("model.json", "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        self.model.save_weights("model.h5")
+        print("Saved model to disk")
+
+    def load_network(self): # loads a network from saved files
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        self.model = model_from_json(loaded_model_json)
+        # load weights into new model
+        self.model.load_weights("model.h5")
+        print("Loaded model from disk")
